be38273875f35a305ec60979f74444ce006df121
pa4373 <pa4373@gmail.com>
2015-09-09 03:25:28 +0800

    Implement NTUEmailValidator class

---
+++ ldap_setup/src/main/java/tw/edu/ntu/im/sdm/sdmtools/ldap_setup/ui/NTUEmailValidator.java
@@ -0,0 +1,22 @@
+package tw.edu.ntu.im.sdm.sdmtools.ldap_setup.ui;
+
+import com.vaadin.data.Validator;
+import com.vaadin.data.validator.EmailValidator;
+
+public class NTUEmailValidator extends EmailValidator {
+
+    public NTUEmailValidator(String errorMessage) {
+        super(errorMessage);
+    }
+
+    @Override
+    public void validate(Object value) {
+        super.validate(value);
+        if (value instanceof String) {
+            if (((String) value).matches("^(\\S+)@ntu.edu.tw$")) {
+                return;
+            }
+        }
+        throw new Validator.InvalidValueException(super.getErrorMessage());
+    }
+}
